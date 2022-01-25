578a2cf8789825847b559d60802cd3a5ad2ca683
marc-hauck <93655278+marc-hauck@users.noreply.github.com>
2022-01-25 12:05:43 +0100

    fixed step size

--- GoalFinder.py
+++ GoalFinder.py
@@ -27,20 +27,18 @@ class GoalFinder(gym.GoalEnv):
                                               "desired_goal": spaces.Box(low=np.array([-1.0, -1.0]),
                                                                          high=np.array([1, 1]))
                                               })
-        self.basis_pts = generate_random_basis(n_points=self.num_bps, n_dims=2)
-        self.agent_size = 0.05
+        self.basis_pts = generate_random_basis_square(n_points=self.num_bps, n_dims=2)
         self.agent_state = None
         self.obs_state = None
         self.goal = None
         self.obstacles = None
-        self.obs_sizes = None
         self.start_state = None
         self.timesteps = timesteps
         self.object_size = 0.04
         self.current_step = 0
         self.fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-        self.out = cv2.VideoWriter('vary.avi', self.fourcc, 10.0, (400, 400))
-        self.punishment = -0.2
+        self.out = cv2.VideoWriter('output1m.avi', self.fourcc, 10.0, (400, 400))
+        self.punishment = - 0.2
         self.payout = 100
         self.reset()
 
@@ -51,43 +49,34 @@ class GoalFinder(gym.GoalEnv):
         self.current_step += 1
         self.agent_state = self.observation_space["achieved_goal"].sample()
         self.obstacles = (np.random.rand(self.num_obstacles*2,) - 0.5) * 2
-        """
-        self.obs_sizes = np.random.randint(1, 5, (self.num_obstacles,)) / 100
-        """
-        #self.obs_sizes[-1] = 0.2
         self.goal = self.observation_space["desired_goal"].sample()
-        collisions = [self.check_collision(self.obstacles[i:i + 2], self.goal) \
-                      for i in (np.arange(self.num_obstacles)) * 2]
-        """
-        =======For varying obj sizes=====
-        collisions = [self.check_collision(self.obstacles[i:i+2], self.goal, margin=self.obs_sizes[int(i/2)]) \
-                      for i in (np.arange(self.num_obstacles))*2]
-        """
+        collisions = [self.check_collision(self.obstacles[i:i+2], self.goal) for i in (np.arange(self.num_obstacles))*2]
+
         while not np.all(collisions):
             self.goal = self.observation_space["desired_goal"].sample()
-            collisions = [self.check_collision(self.obstacles[i:i + 2], self.goal) \
-                          for i in (np.arange(self.num_obstacles)) * 2]
-            #collisions = [self.check_collision(self.obstacles[i:i+2], self.goal, margin=self.obs_sizes[int(i/2)]) \
-                          #for i in (np.arange(self.num_obstacles))*2]
+            collisions = [self.check_collision(self.obstacles[i:i+2], self.goal) for i in (np.arange(self.num_obstacles))*2]
 
         self.obs_state, idx = encode(np.expand_dims(self.obstacles.reshape((self.num_obstacles, 2)), axis=0),
-                                     n_bps_points=self.num_bps, custom_basis=basis)
+                                n_bps_points=self.num_bps, custom_basis=basis)
         self.obs_state = np.squeeze(np.transpose(self.obs_state), axis=-1)
-        """
-        ======= For varying obj sizes========
-        idx = np.squeeze(idx, axis=-1)
-        for j in range(self.num_bps):
-            self.obs_state[j] += self.obs_sizes[idx[j]]
         #print(f"Sampling done: {self.current_step}/{self.timesteps}")
-        """
         ob = self.get_obs()
+
         return ob
 
     def step(self, action):
-        self.agent_state[0] += action[0] # * 2 * self.object_size
-        self.agent_state[1] += action[1] # * 2 * self.object_size
+        x_axis = [1, 0]
+        ac = action
+        norm_action = action / np.linalg.norm(action)
+        angle = np.sign(norm_action[1]) * np.arccos(np.clip(np.dot(x_axis, norm_action), -1.0, 1.0))
+        step = [0, 0]
+        step[0] = np.cos(angle) * 2 * self.object_size
+        step[1] = np.sin(angle) * 2 * self.object_size
+
+        self.agent_state[0] += step[0]
+        self.agent_state[1] += step[1]
         self.agent_state = np.clip(self.agent_state, -1, 1)
-        self.current_step += 1
+
         info = {"collision": 0,
                 "similarity": 0}
         similarity = encode(np.expand_dims(self.agent_state.reshape((1, 2)), axis=0),
@@ -95,14 +84,9 @@ class GoalFinder(gym.GoalEnv):
         info["similarity"] = similarity
 
         for i in (np.arange(self.num_obstacles)) * 2:
-            if self.check_collision(self.agent_state, self.obstacles[i:i+2], sampling=False):
+            if self.check_collision(self.agent_state, self.obstacles[i:i + 2], sampling=False):
                 info["collision"] = 1
-            """
-            ===== F V O S ======
-            if self.check_collision(self.agent_state, self.obstacles[i:i+2],
-                                    margin=self.obs_sizes[int(i/2)], sampling=False):
-                info["collision"] = 1
-            """
+
         obs = self.get_obs()
         reward = self.compute_reward(obs["achieved_goal"], obs["desired_goal"], info)
         done = True if (np.linalg.norm(obs["achieved_goal"] - obs["desired_goal"]) <= 4 * self.object_size) else False
@@ -112,33 +96,31 @@ class GoalFinder(gym.GoalEnv):
     def compute_reward(self, observation, desired_goal, info):
 
         if info["collision"] == 1:
-            return float(-100)
+            return float(-200)
         elif np.linalg.norm(observation - desired_goal) <= 4 * self.object_size:
             return float(1000)
         else:
-            return float(-np.linalg.norm(observation - desired_goal)) * 10
+            return float(-np.linalg.norm(observation - desired_goal)) * 50
 
-        """
-        return self.punishment / np.linalg.norm(np.transpose(info["similarity"]) - np.expand_dims(self.obs_state, axis=-1), ord=2) + \
-               int(np.linalg.norm(observation - desired_goal) <= 0.08) * self.payout - int(info["collision"]) * 100
-        """
+        #return self.punishment * np.linalg.norm(np.transpose(info["similarity"]) - np.expand_dims(self.obs_state, axis=-1), ord=np.inf) + \
+         #      int(np.linalg.norm(observation - desired_goal) <= 0.6) * self.payout
 
     def render(self, mode="human"):
         image = np.ones((400, 400, 3), dtype=np.uint8) * 255
         image = cv2.circle(img=image,
                            center=(int((self.agent_state[0] + 1) * 200), int((self.agent_state[1] +1) * 200)),
-                           radius=int(self.agent_size * 200),
+                           radius=8,
                            color=(255, 0, 0),
                            thickness=-1)
         image = cv2.circle(img=image,
                            center=(int((self.goal[0] +1) * 200), int((self.goal[1] +1) * 200)),
-                           radius=int(self.agent_size * 200),
+                           radius=8,
                            color=(0, 0, 255),
                            thickness=-1)
         for i in (np.arange(self.num_obstacles))*2:
             image = cv2.circle(img=image,
                                center=(int((self.obstacles[i] + 1) * 200), int((self.obstacles[i+1] + 1) * 200)),
-                               radius=int(self.object_size * 200), # int(self.obs_sizes[int(i/2)] * 200),
+                               radius=8,
                                color=(0, 255, 0),
                                thickness=-1)
         for i in range(self.num_bps):
@@ -164,21 +146,13 @@ class GoalFinder(gym.GoalEnv):
 
     def check_collision(self, arr1, arr2, sampling=True):
         if sampling:
-            return np.linalg.norm(arr1 - arr2) >= 4 * (self.object_size)
+            return np.linalg.norm(arr1 - arr2) >= 4 * self.object_size
         else:
-            return np.linalg.norm(arr1 - arr2) <= 2 * (self.object_size)
-    """
-    =========For varying obj sizes===========
-    def check_collision(self, arr1, arr2, margin, sampling=True):
-        if sampling:
-            return np.linalg.norm(arr1 - arr2) >= 2 * (margin + self.agent_size)
-        else:
-            return np.linalg.norm(arr1 - arr2) <= (margin + self.agent_size)
-    """
+            return np.linalg.norm(arr1 - arr2) <= 2 * self.object_size
 
-timesteps = 500000
-num_obstacles = 20
-num_bps = 60
+timesteps = 700000
+num_obstacles = 10
+num_bps = 25
 
 env = GoalFinder(num_obstacles=num_obstacles, timesteps=timesteps, num_bps=num_bps)
 
@@ -186,10 +160,9 @@ env = GoalFinder(num_obstacles=num_obstacles, timesteps=timesteps, num_bps=num_b
 
 obs = env.reset()
 model = PPO('MultiInputPolicy', env, verbose=1).learn(timesteps)
-
 frames = []
 n_steps = 30
-n_runs = 1000
+n_runs = 100
 n_success = 0
 n_collision = 0
 for run in range(n_runs):
@@ -214,10 +187,11 @@ for run in range(n_runs):
             n_collision += 1
             env.reset()
             break
+            #break
         print("##################################################################")
         print("\n\n")
         env.render(mode='human')
-        time.sleep(0.05)
+        time.sleep(0.1)
         if done:
             if run == n_runs - 1:
                 env.reset()
@@ -239,9 +213,8 @@ print(f"Negative reward coefficient: {env.punishment} (Multiplied with L-inf nor
 print(f"Positive reward: {env.payout} (Multiplied with I(goal_reached))")
 print("#####################################################################\n\n")
 
-
-with open('logs.txt', 'a') as logs:
-    logs.write("\n\n########################## OBJ SIZE ###############################\n")
+with open('logs_marc.txt', 'a') as logs:
+    logs.write("\n\n########################## END RESULT ###############################\n")
     logs.write(rundatestr)
     logs.write(f"\nWhile learning, {env.current_step} out of {timesteps} episodes ended in success.\n")
     logs.write(f"During evaluation, in {n_success} of {n_runs} runs the agent managed to reach the goal while colliding {n_collision} times.\n")
